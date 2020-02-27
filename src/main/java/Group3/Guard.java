3519de17efe7d2b0124eadd74e21c69ec741f33b
Merlin Köhler <merlin-koehler@outlook.com>
2020-02-27 14:27:45 +0100

    Added Guard and Intruder classes. Added test map.

---
+++ src/main/java/Group3/Guard.java
@@ -0,0 +1,14 @@
+package Group3;
+
+import Interop.Action.DropPheromone;
+import Interop.Action.GuardAction;
+import Interop.Percept.GuardPercepts;
+
+public class Guard implements Interop.Agent.Guard {
+
+	@Override
+	public GuardAction getAction(GuardPercepts percepts) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
