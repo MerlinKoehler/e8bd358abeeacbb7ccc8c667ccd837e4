2cd84f17f7ae2cf9cd64ee60145d06b6baf59e31
Merlin Köhler <merlin-koehler@outlook.com>
2020-02-27 11:14:25 +0100

    Added static object template.

--- src/main/java/Group3/MainControl.java
+++ src/main/java/Group3/MainControl.java
@@ -1,9 +1,27 @@
 package Group3;
 
+import java.util.ArrayList;
+
 /*
  * We can start the controller from here.
  */
 
 public class MainControl {
 
+	
+	ArrayList<StaticObject> staticObjects = new ArrayList<StaticObject>();
+	
+	public MainControl() {
+		// Read map file
+		// Load static objects
+		// Get initial position of agents
+	}
+	
+	public void doStep() {
+		// do one step for one agent
+	}
+	
+	public ArrayList<StaticObject> getStaticObject(){
+		return staticObjects;
+	}
 }
---
+++ src/main/java/Group3/Wall.java
@@ -0,0 +1,28 @@
+package Group3;
+
+public class Wall extends StaticObject {
+	
+	private int x1;
+	public int getX1() {
+		return x1;
+	}
+
+	private int x2;
+	public int getX2() {
+		return x2;
+	}
+	
+	private int y1;
+	public int getY1() {
+		return y1;
+	}
+	
+	private int y2;
+	public int getY2() {
+		return y2;
+	}
+	
+	public Wall(int x1, int x2, int y1, int y2) {
+		
+	}
+}
