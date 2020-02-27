ed29880781da46381aecd89565cf69fad8a42f1c
Janneke 13 <j.vanbaden@ziggo.nl>
2020-02-27 11:32:50 +0100

    added static objects

---
+++ src/main/java/Group3/SentryTower.java
@@ -0,0 +1,104 @@
+package Group3;
+
+public class SentryTower extends StaticObject {
+
+	private int x1;
+	public int getX1() {
+		return x1;
+	}
+
+	private int x2;
+	public int getX2() {
+		return x2;
+	}
+	
+	private int x3;
+	public int getX3() {
+		return x3;
+	}
+	
+	private int x4;
+	public int getX4() {
+		return x4;
+	}
+	
+	private int y1;
+	public int getY1() {
+		return y1;
+	}
+	
+	private int y2;
+	public int getY2() {
+		return y2;
+	}
+	
+	private int y3;
+	public int getY3() {
+		return y3;
+	}
+	
+	private int y4;
+	public int getY4() {
+		return y4;
+	}
+	
+	private int ix1;
+	public int getIX1() {
+		return ix1;
+	}
+	
+	private int ix2;
+	public int getIX2() {
+		return ix2;
+	}
+	
+	private int ix3;
+	public int getIX3() {
+		return ix3;
+	}
+	
+	private int ix4;
+	public int getIX4() {
+		return ix4;
+	}
+	
+	private int iy1;
+	public int getIY1() {
+		return iy1;
+	}
+	
+	private int iy2;
+	public int getIY2() {
+		return iy2;
+	}
+	
+	private int iy3;
+	public int getIY3() {
+		return iy3;
+	}
+	
+	private int iy4;
+	public int getIY4() {
+		return iy4;
+	}
+	
+	public SentryTower(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, int ix1, int iy1, int ix2, int iy2, int ix3, int iy3, int ix4, int iy4) {
+		this.x1 = x1;
+		this.x2 = x2;
+		this.x3 = x3;
+		this.x4 = x4;
+		this.y1 = y1;
+		this.y2 = y2;
+		this.y3 = y3;
+		this.y4 = y4;
+		
+		this.ix1 = ix1;
+		this.ix2 = ix2;
+		this.ix3 = ix3;
+		this.ix4 = ix4;
+		this.iy1 = iy1;
+		this.iy2 = iy2;
+		this.iy3 = iy3;
+		this.iy4 = iy4;
+	}
+}
---
+++ src/main/java/Group3/Window.java
@@ -0,0 +1,56 @@
+package Group3;
+
+public class Window extends StaticObject{
+
+	private int x1;
+	public int getX1() {
+		return x1;
+	}
+
+	private int x2;
+	public int getX2() {
+		return x2;
+	}
+	
+	private int x3;
+	public int getX3() {
+		return x3;
+	}
+	
+	private int x4;
+	public int getX4() {
+		return x4;
+	}
+	
+	private int y1;
+	public int getY1() {
+		return y1;
+	}
+	
+	private int y2;
+	public int getY2() {
+		return y2;
+	}
+	
+	private int y3;
+	public int getY3() {
+		return y3;
+	}
+	
+	private int y4;
+	public int getY4() {
+		return y4;
+	}
+	
+	public Window(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
+		this.x1 = x1;
+		this.x2 = x2;
+		this.x3 = x3;
+		this.x4 = x4;
+		this.y1 = y1;
+		this.y2 = y2;
+		this.y3 = y3;
+		this.y4 = y4;
+	}
+	
+}
