7fefc5ecb58cb8f756828a5c5ff82bd3269a891b
Margarita <margarita.naryzhnyaya@gmail.com>
2020-03-02 23:18:32 +0100

    Basic visualization

--- src/main/java/Group3/MapSize.java
+++ src/main/java/Group3/MapSize.java
@@ -6,14 +6,36 @@ public class MapSize extends StaticObject{
 	private int getHeight() {
 		return height;
 	}
-
 	private int width;
 	private int getWidth() {
 		return width;
 	}
-	
 	public MapSize(int height, int width) {
 		this.height = height;
 		this.width = width;
+	}
+	int getX1() {
+		return 0;
+	}
+	int getX2() {
+		return 0;
+	}
+	int getX3() {
+		return 0;
+	}
+	int getX4() {
+		return 0;
+	}
+	int getY1() {
+		return 0;
+	}
+	int getY2() {
+		return 0;
+	}
+	int getY3() {
+		return 0;
+	}
+	int getY4() {
+		return 0;
 	}
 }
